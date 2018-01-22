7219490fa50fa6e7f12729fe67c419dbdc386148
mfiari <fiarimike@yahoo.fr>
2018-01-22 21:33:22 +0100

    initial commit

---
+++ Assets/Scripts/LangueScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LangueScript : MonoBehaviour {
+
+    public GameObject flag;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
